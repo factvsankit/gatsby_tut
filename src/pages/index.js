4c087d4297a51be74e65eca57aa047cc52cd0d54
Ankit Sigdel <mta.ankit@gmail.com>
2017-10-29 15:39:21 +0545

    no message

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,6 @@ import Link from "gatsby-link"
 
 export default () =>
 <div style={{ color: `black` }}>
-<h1> Wes, are youd still around?</h1>
+<h1> Wes, are you still around?</h1>
 <Link to="/counter/">Link</Link>
 </div>
