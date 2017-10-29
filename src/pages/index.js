94da311b09ea985f22dddb2e938293a7cc128681
Ankit Sigdel <mta.ankit@gmail.com>
2017-10-29 15:35:04 +0545

    no message

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,7 @@ import Link from "gatsby-link"
 
 
 export default () =>
-<div style={{ color: `red` }}>
-<h1>Hello World!</h1>
-<p>What a world.</p>
-<img src="https://www.skandium.com/blog/wp-content/uploads/2012/01/Helsinki-800x3001.jpg" />
+<div style={{ color: `black` }}>
+<h1> Wes, are youd still around?</h1>
 <Link to="/counter/">Link</Link>
 </div>
